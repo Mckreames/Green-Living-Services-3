1f6bd23f1b5cb6015fe093eb6ac41e1dee7eeaa9
Mckreames <114367512+Mckreames@users.noreply.github.com>
2023-09-21 16:27:18 -0400

    got all the images for the new carousel lined up. bout to start javascript

--- Green Living Services 3!.js
+++ Green Living Services 3!.js
@@ -1,15 +1,15 @@
-`use strict`
+`use strict`;
 
 let prevScrollPos = window.pageYOffset;
 
 window.addEventListener("scroll", () => {
   const currentScrollPos = window.pageYOffset;
-  
+
   if (prevScrollPos > currentScrollPos) {
     document.querySelector(".head-stuff").style.transform = "translateY(0)";
   } else {
     document.querySelector(".head-stuff").style.transform = "translateY(-100%)";
   }
-  
+
   prevScrollPos = currentScrollPos;
 });
