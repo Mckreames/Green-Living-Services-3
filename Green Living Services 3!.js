db664fe08c4573beeac960329549ae85ee4b0ace
Mckreames <114367512+Mckreames@users.noreply.github.com>
2023-10-24 16:31:35 -0400

    trying to fix carousel. not really working

--- Green Living Services 3!.js
+++ Green Living Services 3!.js
@@ -13,3 +13,37 @@ window.addEventListener("scroll", () => {
 
   prevScrollPos = currentScrollPos;
 });
+
+//Carousel controls
+
+let leftArrow = document.querySelector(".left-arrow");
+let rightArrow = document.querySelector(".right-arrow");
+let carouselImg = document.querySelector(".carousel-img1");
+let currentScrollPos = 1;
+
+let rightScroll = function (currentScrollPos) {
+  if (currentScrollPos == 6) {
+    currentScrollPos = 1;
+    carouselImg.textContent = `carousel-img${currentScrollPos}`;
+    return carouselImg;
+  } else {
+    currentScrollPos++;
+    carouselImg.textContent = `carousel-img${currentScrollPos}`;
+    return carouselImg;
+  }
+};
+
+let leftScroll = function (currentScrollPos) {
+  if (currentScrollPos == 1) {
+    currentScrollPos = 6;
+    carouselImg.textContent = `carousel-img${currentScrollPos}`;
+    return carouselImg;
+  } else {
+    currentScrollPos--;
+    return carouselImg;
+  }
+};
+
+leftArrow.addEventListener("click", leftScroll);
+
+rightArrow.addEventListener("click", rightScroll);
