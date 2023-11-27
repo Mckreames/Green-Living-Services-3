79af05d02d3f25d79d698c7f5cd51fb104b51fd9
Mckreames <114367512+Mckreames@users.noreply.github.com>
2023-11-27 09:12:06 -0500

    trying to make the phone number field auto formatting but the code is fighting back

--- Green Living Services 3!.js
+++ Green Living Services 3!.js
@@ -47,3 +47,26 @@ let leftScroll = function (currentScrollPos) {
 leftArrow.addEventListener("click", leftScroll);
 
 rightArrow.addEventListener("click", rightScroll);
+
+// JavaScript function to format the phone number
+function formatPhoneNumber() {
+  // Get the input element
+  var phoneNumberInput = document.getElementById("phoneNumber");
+
+  // Get the input value and remove non-numeric characters
+  var phoneNumber = phoneNumberInput.value.replace(/\D/g, "");
+
+  // Check if the phone number is not empty
+  if (phoneNumber.length > 0) {
+    // Format the phone number as (XXX) XXX-XXXX
+    phoneNumber = phoneNumber.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3");
+
+    // Set the formatted value back to the input
+    phoneNumberInput.value = phoneNumber;
+  }
+}
+
+// Attach the formatPhoneNumber function to the input's blur event
+document
+  .getElementById("phoneNumber")
+  .addEventListener("blur", formatPhoneNumber);
